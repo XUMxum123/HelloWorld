63c434851a61c933086419f85022774de51380a8
meng.xu <18850337241@163.com>
2016-08-28 19:15:41 +0800

    add function.php => maybe not use now

---
+++ common/function.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Table or other  ##define
+* @author: meng.xu
+* @time: 2016.04.25
+*
+* */
+
+// nbateam table define  --- meng.xu
+define('DB_NBA_TEAM',     'nbateam');
+define('DB_NBA_ID',     'Id');
+define('DB_NBA_NAME',     'Name');
+define('DB_NBA_LOGO',     'Logo');
+define('DB_NBA_WIN',     'Win');
+define('DB_NBA_LOSE',     'Lose');
+define('DB_NBA_RANK',     'Rank');
+define('DB_NBA_ALLIANCE',     'Alliance');
+define('DB_NBA_PLAYOFFS',     'Playoffs');
+define('DB_NBA_PARTITION',     'Partition');
+
+
+/*
+ * common function for Checklist module
+* */
+    function getMyUrl(){
+	  $url = "http://www.xum.com/nbateam/火箭.png";
+	  return $url;
+    }
+
+
