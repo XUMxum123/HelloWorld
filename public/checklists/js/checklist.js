092e21da52d102c19e1ddd7de0e2675fe041b0a1
meng.xu <18850337241@163.com>
2016-08-31 22:18:11 +0800

    add layer js file and modify checklist.js

--- public/checklists/js/checklist.js
+++ public/checklists/js/checklist.js
@@ -5,3 +5,11 @@
  * @time 2016.08.20
  *
  */
+
+ $(document).ready(function(){
+    $("button.submit").click(function(){
+    	layer.msg('success');
+    	//alert("click me!");
+          //window.location.href = "index"; // go to indexAction()
+    });
+ });
