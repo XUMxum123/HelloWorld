d82de6d4676e07c3620bf9daed72b3b3ed38e604
meng.xu <18850337241@163.com>
2016-09-02 22:10:12 +0800

    modify checklist.js, add ajax() for xumadd method

--- public/checklists/js/checklist.js
+++ public/checklists/js/checklist.js
@@ -22,26 +22,19 @@
     var $name = $("input[name='name']").val();
     var $sex = $("select.sex option:selected").val();  // var $sex = $('select.sex option:selected').text();
     var $country = $("select[name='country'] option[selected]").val(); // var $country = $("select[name='country'] option[selected]").text();
-    var $nbateam = $("select.nbateam").find("option:selected").val(); //var $nbateam = $("select.nbateam").find("option:selected").text();
-    var $url = "xumadd?title=" + $title + "&content=" + $content;
+    var $nbateamid = $("select.nbateam").find("option:selected").val(); //var $nbateam = $("select.nbateam").find("option:selected").text();
+/*    var $url = "xumadd?title=" + $title + "&content=" + $content;
         $url += "&name=" + $name + "&sex=" + $sex;
         $url += "&country=" + $country + "&nbateam=" + $nbateam;
-    window.location.href = $url;
-/*     $.ajax({
+        window.location.href = $url;*/
+     $.ajax({
         	type: "POST",
-        	 url: "xumadd",
-        	data: {
-                 "title": $title,
-               "content": $content,
-                  "name": $name,
-                   "sex": $sex,
-               "country": $country,
-               "nbateam": $nbateam
-        	},
+        	 url: "xumadd" + '?title=' + $title + '&content=' + $content + '&name=' + $name
+ 	               + '&sex=' + $sex + '&country=' + $country + '&nbateamid=' + $nbateamid,
         success: function(){
-            //alert("123");
+            alert("保存成功!");
          }
-        });*/
+        });
     });
  });
 
