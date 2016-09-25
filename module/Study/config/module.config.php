aeb27abcd5b2d363240177d3ff0a4faf775d7c34
meng.xu <18850337241@163.com>
2016-09-25 19:47:04 +0800

    delete /../view/layout/layout.phtml,

--- module/Study/config/module.config.php
+++ module/Study/config/module.config.php
@@ -57,10 +57,12 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
+          /*
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'study/study/index' => __DIR__ . '/../view/study/study/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            */
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
